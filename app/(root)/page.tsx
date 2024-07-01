cf78e46ddfd10c4040e12ab9f71ecd72a2aabdf8
OtaraAlex <mbechealex61@gmail.com>
2024-07-01 16:30:32 +0300

    implement the homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,25 @@
-import { Button } from "@/components/ui/button"
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
-      <section className='flex flex-col gap-5'>
-        <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
-        <Button className="text-white-1 bg-orange-1">Button</Button>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
+              key={id}
+              imgURL={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

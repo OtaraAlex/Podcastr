4a29f737af1324c56a68dc0bce9f4c75f4e7e178
OtaraAlex <mbechealex61@gmail.com>
2024-07-03 10:07:21 +0300

    setup clerk authentication

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://boss-adder-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;

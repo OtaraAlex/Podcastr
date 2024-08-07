3624095d400f6e498643e97efc0aeee15e31b849
OtaraAlex <mbechealex61@gmail.com>
2024-08-07 17:04:02 +0300

    add podcast details page

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "clean-axolotl-158.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
